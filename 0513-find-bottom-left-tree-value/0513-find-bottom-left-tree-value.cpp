576ca7f1f79685d887da515c2d98afdb234f7ba9
Phani Bhusan Mohanty <mohantyphanibhusan@gmail.com>
2024-08-25 03:24:57 +0530

    Update 0513-find-bottom-left-tree-value.cpp

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -9,27 +9,56 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//BFS SOLUTION WITHOUT CALCULATING THE HEIGHT
+
+// class Solution {
+// public:
+//     int findBottomLeftValue(TreeNode* root) {
+//        int ans;
+//        queue<TreeNode*> q;
+//        q.push(root);
+//        while(!q.empty()){
+//         ans = q.front()->val;
+//         int size = q.size();
+//         while(size--){
+//           TreeNode* temp = q.front();
+//           q.pop();
+//           if(temp->left){
+//             q.push(temp->left);
+//           }
+//           if(temp->right){
+//             q.push(temp->right);
+//           }
+//         }
+//        }
+
+//        return ans; 
+//     }
+// };
+
+//DFS SOLUTION
+
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) {
-       int ans;
-       queue<TreeNode*> q;
-       q.push(root);
-       while(!q.empty()){
-        ans = q.front()->val;
-        int size = q.size();
-        while(size--){
-          TreeNode* temp = q.front();
-          q.pop();
-          if(temp->left){
-            q.push(temp->left);
-          }
-          if(temp->right){
-            q.push(temp->right);
-          }
+    int ans;
+    void helperFunc(TreeNode* root, int currLevel,int& maxlevel){
+        if(!root){
+            return;
         }
-       }
+        if(currLevel >= maxlevel){
+          maxlevel = currLevel;
+          ans = root->val;
+        }
+        helperFunc(root->right,currLevel+1,maxlevel);
+        helperFunc(root->left,currLevel+1,maxlevel);
+        
+    }
 
-       return ans; 
+    int findBottomLeftValue(TreeNode* root) {
+        int maxlevel = 0;
+       helperFunc(root,0,maxlevel);
+
+       return ans;
     }
 };
